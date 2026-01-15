e8b76d892d974a373d697674c3cd03d734e29add
Mashrafi Anam <mashrafianam99@gmail.com>
2026-01-15 20:27:14 +0600

    Change app.run to host '0.0.0.0' and port 5000
    Updated the application to run on host '0.0.0.0' and port 5000.

--- expense-tracker/app.py
+++ expense-tracker/app.py
@@ -50,4 +50,6 @@ def index():
     return render_template('index.html', expenses=expenses, total=total)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
+
+
